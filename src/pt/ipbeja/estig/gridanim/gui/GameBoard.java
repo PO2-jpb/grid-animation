c3ad3253b69f71d69635b2da5d28fb03681e96eb
João Paulo Barros <joao.barros@ipbeja.pt>
2021-05-31 22:03:50 +0100

    first stable version

--- src/pt/ipbeja/estig/gridanim/gui/GameBoard.java
+++ src/pt/ipbeja/estig/gridanim/gui/GameBoard.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
